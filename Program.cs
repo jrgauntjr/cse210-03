a813b63c7f9050ed0395d4d71a77c03aa06fb28e
jrgauntjr <gau20002@byui.edu>
2022-10-13 15:54:06 -0600

    new file:   .vscode/launch.json 	new file:   .vscode/tasks.json 	new file:   Program.cs 	new file:   cse210-03.csproj 	new file:   game/Director.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Lab03.Game;
+
+namespace Lab03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Director director = new Director();
+            director.StartGame();
+        }
+    }
+}
---
+++ game/Director.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace Lab03.Game
+{
+    public class Director
+    {
+        public void StartGame()
+        {
+
+        }
+    }
+}
