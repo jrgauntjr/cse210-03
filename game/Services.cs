44bb54240d4c69b640dc3059505f7daee2568c0b
Noahlarsen17 <lar18027@byui.edu>
2022-10-20 16:13:22 -0600

    modified:   game/Services.cs

--- game/Services.cs
+++ game/Services.cs
@@ -8,6 +8,7 @@ namespace Lab03.Game
 {
     public class Services {
 
+        public static void Main();
         string[] words = { "calculator", "phone", "glasses", "pencil", "grape", "duck", "crawl", "parachute", "sphinx", "motherhood"};
 
         Random rand = new Random();
