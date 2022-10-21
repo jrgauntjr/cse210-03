09be43745dc7ca382f67234a94b319cdac21481a
jrgauntjr <gau20002@byui.edu>
2022-10-21 11:06:49 -0600

    modified:   game/Guesser.cs 	modified:   game/Services.cs

--- game/Guesser.cs
+++ game/Guesser.cs
@@ -5,39 +5,41 @@ namespace Lab03.Game
 {
     public class Guesser
     {
+        Services service = new Services();
+        string ChosenWord = service.getWord();
+        char[] word = ChosenWord.ToCharArray();
+        char[] arrayBlanks;
+        List<char> badLetters = new List<char>();
+        public char[] Blanks(char[] word)
+        {
 
-    string ChosenWord = Services.word();
-    char[] word = ChosenWord.ToCharArray();
-    char[] arrayBlanks;
-    List<char> badLetters = new List<char>();
-    public char[] Blanks(char[] word){
-
-        for ( int runs = 0; runs< word.Length; runs ++ )
+            for ( int runs = 0; runs< word.Length; runs ++ )
             {
                 arrayBlanks[runs] = '_';
             }
-
             return arrayBlanks;
         }
 
-    public bool CheckLetter (char letter){
-
-        bool stat_flag = false;
-
-        for ( int runs = 0; runs< word.Length; runs ++)
+        public bool CheckLetter (char letter)
         {
-            if (letter == word[runs]){
-                arrayBlanks[runs] = letter;
-                stat_flag = true;
-            }
-            else{
-                badLetters.Add(letter);
-            }
 
+            bool stat_flag = false;
+
+            for ( int runs = 0; runs< word.Length; runs ++)
+            {
+                if (letter == word[runs])
+                {
+                    arrayBlanks[runs] = letter;
+                    stat_flag = true;
+                }
+                else
+                {
+                    badLetters.Add(letter);
+                }
+
+            }
+            return stat_flag;
         }
-
-        return stat_flag;
-    }
 
 
     }
